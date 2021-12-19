b67121aae1ce0be58635f67ed1565a0ebf29824b
Yunuskhon Egamberdiev <yunusxon@gmail.com>
2021-12-19 13:22:31 +0500

    part6

--- src/containers/PeoplePage/PeoplePage.jsx
+++ src/containers/PeoplePage/PeoplePage.jsx
@@ -3,26 +3,37 @@ import { getApiResource } from '../../utils/network';
 import { API_PEOPLE } from '../../constants/api';
 import { getPeopleId, getpeopleImage } from '../../services/getPeopleData';
 import PeopleList from '../../components/PeoplePage/PeopleList';
+import { withErrorApi } from '../../hoc-helpers/withErrorApi';
 import styles from './PeoplePage.module.css';
 
-const PeoplePage = () => {
+const PeoplePage = ({ setErrorApi }) => {
 	const [people, setPeople] = useState(null);
 
 	const getResource = async (url) => {
 		const res = await getApiResource(url);
 
-		const peopleList = res.results.map(({ name, url }) => {
-			const id = getPeopleId(url);
-			const img = getpeopleImage(id);
-			return { name, id, img };
-		});
-		setPeople(peopleList);
+		if (res) {
+			const peopleList = res.results.map(({ name, url }) => {
+				const id = getPeopleId(url);
+				const img = getpeopleImage(id);
+				return { name, id, img };
+			});
+			setPeople(peopleList);
+			setErrorApi(false);
+		} else {
+			setErrorApi(true);
+		}
 	};
 
 	useEffect(() => {
 		getResource(API_PEOPLE);
 	}, []);
-	return <>{people && <PeopleList people={people} />}</>;
+	return (
+		<>
+			<h1>Navigation</h1>
+			{people && <PeopleList people={people} />}
+		</>
+	);
 };
 
-export default PeoplePage;
+export default withErrorApi(PeoplePage);
---
+++ src/hoc-helpers/withErrorApi.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import ErrorMessage from '../components/ErrorMessage';
+
+export const withErrorApi = (View) => {
+	return (props) => {
+		const [errorApi, setErrorApi] = useState(false);
+		return <>{errorApi ? <ErrorMessage /> : <View setErrorApi={setErrorApi} {...props} />}</>;
+	};
+};
