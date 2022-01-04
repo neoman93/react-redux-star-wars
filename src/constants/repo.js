e5729fde755b9279d0a80f6e5cfd521425f55ec1
Yunuskhon Egamberdiev <yunusxon@gmail.com>
2022-01-04 10:50:41 +0500

    add deploy scripts

---
+++ src/constants/repo.js
@@ -0,0 +1,1 @@
+export const REPO_NAME = 'react-redux-star-wars';
--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Switch, NavLink, Route } from 'react-router-dom';
 import routesConfig from '@routes/routesConfig';
+import { REPO_NAME } from '@constants/repo';
 import Header from '@components/Header/Header';
 import styles from './App.module.css';
 
 const App = () => {
 	return (
 		<>
-			<BrowserRouter>
+			<BrowserRouter basename={`/${REPO_NAME}/`}>
 				<div className={styles.wrapper}>
 					<Header />
 					<Switch>
